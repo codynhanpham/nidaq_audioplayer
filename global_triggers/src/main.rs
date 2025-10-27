993b67a9d2e0a9ce36c3a481980c359c376cd847
codynhanpham <87593612+codynhanpham@users.noreply.github.com>
2025-10-27 13:37:05 -0500

    Remove terminal popup on execute

--- global_triggers/src/main.rs
+++ global_triggers/src/main.rs
@@ -1,3 +1,7 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+
 use enigo::{
     Direction::{Press, Release},
     Enigo, Key, Keyboard, Settings,
